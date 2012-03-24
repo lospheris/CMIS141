5158724d1cd35da0d5d5d1a9baa502b0aea68cce
Dell-Ray Sackett <lospheris@gmail.com>
2012-03-24 10:22:25 +0100

    Compute Area from the book. Initial Commit.

---
+++ ComputeArea/src/ComputeArea.java
@@ -0,0 +1,18 @@
+public class ComputeArea {
+	public static void main(String[] args) {
+		double radius;	// Declare a variable for the radius
+		double area;	// Delcare a variable for the area
+		
+		// Set the radius
+		radius = 20;
+		
+		// compute area
+		area = radius * radius * 3.14159;
+		
+		// Display the results
+		System.out.println("The area of the circle of radius " + radius + " is " + area);
+		
+	}
+	
+}
+	
