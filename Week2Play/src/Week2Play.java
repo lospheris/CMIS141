350d38c906484346dc31756d217bcdec9cb81ef4
Dell-Ray Sackett <lospheris@gmail.com>
2012-03-24 18:52:34 +0100

    Adding Week 2 Play.

---
+++ Week2Play/src/Week2Play.java
@@ -0,0 +1,20 @@
+/*******************************************************************
+ * File Name  : Week2Play.Java
+ * Course Name: UMUC - CMIS 141
+ * Date    :
+ * Compiler   : JDK 6
+ * IDE        : Eclipse Juno 4.2.0
+ * @author Dell-Ray Sackett
+ *
+ * Program Description:
+ 		This program is to practice things that I learn during the course of week 2 of the class.
+ * Testing: Tested the program using ...
+  		Display obtained with testing:
+ ********************************************************************/
+public class Week2Play
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
