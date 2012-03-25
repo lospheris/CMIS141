53194fac53c9324229a0f1f7f39fb41a66440bf9
Dell-Ray Sackett <lospheris@gmail.com>
2012-03-25 20:34:19 +0200

    Adding Week 2 Conference work.

---
+++ Week2Conferences/src/Week2Conferences.java
@@ -0,0 +1,20 @@
+public class Week2Conferences
+{
+	public static void main(String[] args)
+	{
+		int x = 1;
+		System.out.println((true) && (3 > 4));
+		System.out.println(!(x > 0) && (x > 0));
+		System.out.println((x > 0) || (x < 0));
+		System.out.println((x != 0) || (x == 0) );
+		System.out.println((x >= 0) || (x < 0) );
+		System.out.println((x != 1) == !(x == 1) );
+		if (true)
+		{
+			System.out.println("Something");
+		} else
+		{
+			System.out.println("Not somthing");
+		}
+	}
+}
